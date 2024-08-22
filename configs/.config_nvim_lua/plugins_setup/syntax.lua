6940a8c4ff61654599f017066e4287e7a92b3829
joao.neves <joao95neves@gmail.com>
2024-08-22 14:33:38 +0100

    fix(nvim): fix code folding

--- configs/.config_nvim_lua/plugins_setup/syntax.lua
+++ configs/.config_nvim_lua/plugins_setup/syntax.lua
@@ -4,6 +4,7 @@ function SyntaxSetup()
             -- https://github.com/nvim-treesitter/nvim-treesitter
             'nvim-treesitter/nvim-treesitter',
             lazy = true,
+            event = "BufEnter",
             build = function()
                 require('nvim-treesitter.install').update()
             end,
@@ -15,6 +16,12 @@ function SyntaxSetup()
                     },
                     ensure_installed = "all",
                 })
+
+                vim.opt.foldmethod = "expr"
+                vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+                -- Open file unfolded.
+                vim.opt.foldlevel = 99
             end,
         },
 
@@ -22,7 +29,8 @@ function SyntaxSetup()
             -- https://github.com/HiPhish/rainbow-delimiters.nvim
             'HiPhish/rainbow-delimiters.nvim',
             lazy = true,
-        }
+            event = "BufEnter",
+        },
 
     }
 end
